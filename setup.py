877debd2847747ea440468fcd5cc00f300d124ab
Alexander Maximovskih <amaximovskih@haivision.com>
2022-07-08 20:29:57 -0400

    init

---
+++ setup.py
@@ -0,0 +1,14 @@
+'''
+Test lib 2
+'''
+
+from setuptools import setup
+
+setup(
+    author='Demo',
+    name='test-lib-2',
+    version='1.0.0',
+    description='Test lib 2',
+    install_requires=[],
+    py_modules=['test-lib-2']
+)
